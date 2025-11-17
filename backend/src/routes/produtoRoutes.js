5ef9808eb9d9ccc43d81d87516792e1500a50efa
eduhv01 <eduardohv1021@gmail.com>
2025-11-17 16:31:34 -0300

    corrgindo erros

--- backend/src/routes/produtoRoutes.js
+++ backend/src/routes/produtoRoutes.js
@@ -1,5 +1,3 @@
-// backend/src/routes/produtoRoutes.js
-
 const express = require('express');
 const router = express.Router();
 const {
@@ -10,19 +8,15 @@ const {
     deleteProduct
 } = require('../controllers/produtoController');
 
-const { protegerRota } = require('../middleware/autenticacaoMiddleware'); // Importa o middleware de proteção
+const { protegerRota } = require('../middleware/autenticacaoMiddleware');
 
-// Rotas para /api/produtos
-// Todas essas rotas exigem autenticação (protect)
 router.route('/')
-    .post(protegerRota, createProduct) // Rota para criar um produto (POST)
-    .get(protegerRota, getProducts);   // Rota para obter todos os produtos (GET)
+    .post(protegerRota, createProduct) 
+    .get(protegerRota, getProducts);   
 
-// Rotas para /api/produtos/:id
-// Todas essas rotas exigem autenticação (protect)
 router.route('/:id')
-    .get(protegerRota, getProductById)   // <--- CORRIGIDO: Rota para obter um produto por ID (GET)
-    .put(protegerRota, updateProduct)    // Rota para atualizar um produto por ID (PUT)
-    .delete(protegerRota, deleteProduct); // Rota para deletar um produto por ID (DELETE)
+    .get(protegerRota, getProductById)   
+    .put(protegerRota, updateProduct)    
+    .delete(protegerRota, deleteProduct); 
 
-module.exports = router; // Exporta o router para ser usado no server.js
+module.exports = router;
